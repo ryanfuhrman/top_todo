cae7c26598531f729242a47627d568feb246a6ab
Ryan Fuhrman <rsfuhrman10@gmail.com>
2022-02-10 15:25:57 -0500

    so many changes

--- src/index.js
+++ src/index.js
@@ -28,10 +28,14 @@ const model = (() => {
     resetProjectNameList();
   };
 
-  const updateTodo = (updatedTodo, id) => {
+  const updateTodo = (updatedTodo, id, viewFn) => {
     const list = JSON.parse(localStorage.getItem("masterList"));
     let indexOfTodo = list.findIndex((todo) => todo.id === id);
+    console.log(updatedTodo);
     console.log(list[indexOfTodo]);
+    list[indexOfTodo] = updatedTodo;
+    saveListToLocalStorage(list);
+    viewFn();
   };
 
   const saveListToLocalStorage = (list) => {
@@ -49,10 +53,11 @@ const model = (() => {
     return cleanedUpList;
   };
 
-  const createEditButton = () => {
+  const createEditButton = (fn) => {
     const editButton = document.createElement("button");
     editButton.innerHTML = "Edit";
     editButton.classList.add("edit-todo-btn");
+    editButton.addEventListener("click", fn);
     return editButton;
   };
 
@@ -66,10 +71,18 @@ const model = (() => {
 
   const createMoreInfoButton = (fn) => {
     const moreInfoButton = document.createElement("button");
-    moreInfoButton.innerHTML = "more Info";
+    moreInfoButton.innerHTML = "More Info";
     moreInfoButton.classList.add("more-info-button");
     moreInfoButton.addEventListener("click", fn);
     return moreInfoButton;
+  };
+
+  const createDeleteTodoButton = (fn) => {
+    const deleteTodoButton = document.createElement("button");
+    deleteTodoButton.innerHTML = "Delete Todo";
+    deleteTodoButton.classList.add("delete-todo-button");
+    deleteTodoButton.addEventListener("click", fn);
+    return deleteTodoButton;
   };
 
   return {
@@ -82,6 +95,7 @@ const model = (() => {
     updateTodo,
     createLessInfoButton,
     createMoreInfoButton,
+    createDeleteTodoButton,
   };
 })();
 
@@ -95,27 +109,6 @@ const view = (() => {
   const todoList = document.createElement("ul");
   todoList.classList.add("todo-list-ul");
   mainDiv.appendChild(todoList);
-
-  // const addDeleteButton = (element) => {
-  //   const deleteTodoButton = document.createElement("button");
-  //   deleteTodoButton.classList.add("delete-todo-btn");
-  //   deleteTodoButton.innerHTML = "X";
-  //   element.appendChild(deleteTodoButton);
-  // };
-
-  // const addMoreInfoButton = (element) => {
-  //   const moreInfoButton = document.createElement("button");
-  //   moreInfoButton.classList.add("more-info-btn");
-  //   moreInfoButton.innerHTML = "More Info";
-  //   element.appendChild(moreInfoButton);
-  // };
-
-  const addEditButton = (element) => {
-    const editTodoButton = document.createElement("button");
-    editTodoButton.classList.add("edit-todo-btn");
-    editTodoButton.innerHTML = "Edit";
-    element.appendChild(editTodoButton);
-  };
 
   const editableTodo = (todo, id) => {
     const todoToEdit = model.getTodo(id)[0];
@@ -171,25 +164,32 @@ const view = (() => {
   };
 
   const displayTodos = (
-    list = JSON.parse(localStorage.getItem("masterList"))
+    list = JSON.parse(localStorage.getItem("masterList")),
+    moreInfoFn,
+    deleteTodoFn
   ) => {
     todoList.innerHTML = "";
+
+    const moreInfoButton = model.createMoreInfoButton(moreInfoFn);
+    console.log(moreInfoButton);
+    const deleteTodoButton = model.createDeleteTodoButton(deleteTodoFn);
+
     list.map(
       ({ id, title, description, dueDate, priorityLevel, projectName }) => {
         const newTodoLi = document.createElement("li");
         newTodoLi.classList.add("todo-li");
         newTodoLi.id = id;
         const todoHTML = `
-          <ul class="todo-info-list">
-            <li class="todo-title">${title}</li>
-            <li class="todo-description">${description}</li>
-            <li class="todo-date">${dueDate}</li>
-            <li class="todo-priority">${priorityLevel}</li>
-            <li class="todo-project">${projectName}</li>
+          <ul class="todo-data-list" id=${id}>
+            <li class="title todo-data">${title}</li>
+            <li class="description todo-data more-todo-data">${description}</li>
+            <li class="date todo-data more-todo-data">${dueDate}</li>
+            <li class="priority todo-data">${priorityLevel}</li>
+            <li class="project todo-data more-todo-data">${projectName}</li>
           </ul>
           <div class="buttons">
-            <button class="more-info-btn">More Info</button>
-            <button class="delete-todo-btn">Delete Todo</button>
+            ${moreInfoButton.outerHTML}
+            ${deleteTodoButton.outerHTML}
           </div>`;
 
         newTodoLi.innerHTML = todoHTML;
@@ -210,29 +210,21 @@ const view = (() => {
     displayTodos(filteredTodos);
   };
 
-  const displayMoreInfo = (id, li) => {
-    const list = JSON.parse(localStorage.getItem("masterList"));
-    const todoSelected = list.filter((todo) => todo.id === id);
-    li.classList.add("display-more-info");
-    const descriptionDiv = document.createElement("div");
-    descriptionDiv.classList.add("todo-data-div");
-    descriptionDiv.innerHTML = todoSelected[0].description;
-    const dueDateDiv = document.createElement("div");
-    dueDateDiv.classList.add("todo-data-div");
-    dueDateDiv.innerHTML = todoSelected[0].dueDate;
-    const priorityLevelDiv = document.createElement("div");
-    priorityLevelDiv.classList.add("todo-data-div");
-    priorityLevelDiv.innerHTML = todoSelected[0].priorityLevel;
-    const projectNameDiv = document.createElement("div");
-    projectNameDiv.classList.add("todo-data-div");
-    projectNameDiv.innerHTML = todoSelected[0].projectName;
-    const editButton = model.createEditButton();
+  const displayMoreInfo = (todo, lessInfoFn, editFn) => {
+    const todoButtonsDiv = todo.children[1];
+    const moreInfoButton = todoButtonsDiv.children[0];
+    const editButton = model.createEditButton(editFn);
 
-    li.appendChild(descriptionDiv);
-    li.appendChild(dueDateDiv);
-    li.appendChild(priorityLevelDiv);
-    li.appendChild(projectNameDiv);
-    li.appendChild(editButton);
+    todoButtonsDiv.appendChild(editButton);
+    todoButtonsDiv.removeChild(moreInfoButton);
+    const lessInfoButton = model.createLessInfoButton(lessInfoFn);
+    todoButtonsDiv.prepend(lessInfoButton);
+    const todoData = todo.children[0].children;
+    for (let i = 0; i < todoData.length; i++) {
+      if (todoData[i].classList.contains("more-todo-data")) {
+        todoData[i].classList.add("hide-todo-data");
+      }
+    }
   };
 
   const displayLessInfo = (li) => {
@@ -294,31 +286,55 @@ const controller = (() => {
   };
 
   const handleMoreInfo = (e) => {
-    const todo = e.target.parentNode;
-    const moreInfoButton = todo.children[1];
-    const lessInfoButton = model.createLessInfoButton(handleLessInfo);
-    todo.removeChild(moreInfoButton);
-    todo.appendChild(lessInfoButton);
-    const li = todo;
-    const id = li.id;
-    view.displayMoreInfo(id, li);
-    const editInfoButton = document.querySelector(".edit-todo-btn");
-    editInfoButton.addEventListener("click", handleEditTodo);
+    const todoDiv = e.target.parentElement.parentElement;
+    view.displayMoreInfo(todoDiv, handleLessInfo, handleEditTodo);
+    // const moreInfoButton = todoButtonsDiv.children[0];
+    // todoButtonsDiv.removeChild(moreInfoButton);
+    // const lessInfoButton = model.createLessInfoButton(handleLessInfo);
+    // todoButtonsDiv.prepend(lessInfoButton);
+    // const todoData = todoButtonsDiv.parentElement.children[0].children;
+    // for (let i = 0; i < todoData.length; i++) {
+    //   if (todoData[i].classList.contains("more-todo-data")) {
+    //     todoData[i].classList.add("hide-todo-data");
+    //   }
+    // }
+
+    // const moreInfoButton = todo.children[1];
+    // todo.removeChild(moreInfoButton);
+    // todo.appendChild(lessInfoButton);
+    // const li = todo;
+    // const id = li.id;
+    // view.displayMoreInfo(id, li);
+    // const editInfoButton = document.querySelector(".edit-todo-btn");
+    // editInfoButton.addEventListener("click", handleEditTodo);
     applyEventListeners();
   };
 
   const handleLessInfo = (e) => {
-    const parent = e.target.parentNode;
-    view.displayLessInfo(parent);
-    const todo = e.target.parentNode;
-    const lessInfoButton = todo.children[2];
+    const todoButtonsDiv = e.target.parentElement;
+    const lessInfoButton = todoButtonsDiv.children[0];
+    todoButtonsDiv.removeChild(lessInfoButton);
     const moreInfoButton = model.createMoreInfoButton(handleMoreInfo);
-    todo.removeChild(lessInfoButton);
-    todo.appendChild(moreInfoButton);
+    todoButtonsDiv.prepend(moreInfoButton);
+    const todoData = todoButtonsDiv.parentElement.children[0].children;
+    for (let i = 0; i < todoData.length; i++) {
+      if (todoData[i].classList.contains("hide-todo-data")) {
+        todoData[i].classList.remove("hide-todo-data");
+      }
+    }
+
+    // const parent = e.target.parentNode;
+    // view.displayLessInfo(parent);
+    // const todo = e.target.parentNode;
+    // const lessInfoButton = todo.children[2];
+    // const moreInfoButton = model.createMoreInfoButton(handleMoreInfo);
+    // todo.removeChild(lessInfoButton);
+    // todo.appendChild(moreInfoButton);
   };
 
   const handleEditTodo = (e) => {
-    const todo = e.target.parentElement;
+    const todo = e.target.parentElement.parentElement;
+    console.log(todo);
     const id = todo.id;
     view.editableTodo(todo, id);
     const updateTodoButton = document.querySelector("#update-todo-submit");
@@ -333,7 +349,8 @@ const controller = (() => {
     const form = document.getElementById("edit-todo-form");
     const data = new FormData(form);
     const todo = Object.fromEntries(data.entries());
-    model.updateTodo(todo, id);
+    todo.id = id;
+    model.updateTodo(todo, id, view.displayTodos);
   };
 
   const applyEventListeners = () => {
@@ -350,7 +367,11 @@ const controller = (() => {
   };
 
   // Render Todos to Page
-  view.displayTodos(JSON.parse(localStorage.getItem("masterList")));
+  view.displayTodos(
+    JSON.parse(localStorage.getItem("masterList")),
+    handleMoreInfo,
+    handleDeleteTodo
+  );
   applyEventListeners();
   handleDisplayProjectNames();
 })();
